3aef7389d6eefdf4f099433a3a5b4103801461c3
Diego Miguel <eduardo.miguelg99@gmail.com>
2021-06-08 11:29:57 -0500

    Change rock paper scissors logic
    now it works correctly if the user puts a number higher than 2

--- RockPaperScissors/rock_paper_scissors.py
+++ RockPaperScissors/rock_paper_scissors.py
@@ -32,22 +32,26 @@ options = [rock, paper, scissors]
 player = int(input("What do you choose?\nType 0 for Rock, 1 for Paper and 2 for Scissors: "))
 computer = random.randint(0, 2)
 
-print(f"\nYou chose:\n{options[player]}\n\nComputer chose:\n{options[computer]}")
+if player > 2:
+  print("You type an invalid number")
+else:
 
-if player == computer:
-  print("Its a draw")
-elif player == 0:
-  if computer == 2:
-    print("You win")
-  else:
-    print("You lose")
-elif player == 1:
-  if computer == 0:
-    print("You win")
-  else:
-    print("You lose")
-elif player == 2:
-  if computer == 1:
-    print("You win")
-  else:
-    print("You lose")
+  print(f"{options[player]}\n\nComputer chose:\n{options[computer]}")
+
+  if player == computer:
+    print("Its a draw")
+  elif player == 0:
+    if computer == 2:
+      print("You win")
+    else:
+      print("You lose")
+  elif player == 1:
+    if computer == 0:
+      print("You win")
+    else:
+      print("You lose")
+  elif player == 2:
+    if computer == 1:
+      print("You win")
+    else:
+      print("You lose")
